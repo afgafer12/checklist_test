e469bdb98ea0cdb2fd13b522488e340425550f94
afgafer <afgafer1@gmail.com>
2025-04-29 14:41:07 +0700

    COMMIT TELAT perbaikan middleware auth

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', '\App\Http\Controllers\Api\AuthController@login');
 Route::post('/register', '\App\Http\Controllers\Api\AuthController@register');
+Route::get('/unauthorized', '\App\Http\Controllers\Api\AuthController@unauthorized')->name('unauthorized');
 
 Route::middleware('auth:api')->group(function(){
 
